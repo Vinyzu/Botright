7e19deaa381e33313baf7bb1a1602d34dea8c952
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2024-01-17 19:02:29 +0100

    Fixing .gitignore to include tests-assets (Update v0.5)

---
+++ tests/playwright_tests/__init__.py
@@ -0,0 +1,2 @@
+from playwright._impl._path_utils import get_file_dirname
+print(get_file_dirname())
--- tests/test_hcaptcha.py
+++ tests/test_hcaptcha.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.asyncio
 @pytest.mark.xfail
 async def test_solve_hcaptcha(page):
-    await page.goto("https://accounts.hcaptcha.com/demo")
+    await page.goto("https://accounts.hcaptcha.com/demo?sitekey=00000000-0000-0000-0000-000000000000")
 
     result = await page.solve_hcaptcha()
     assert result
